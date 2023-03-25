fa54406b3415461c02f7e836db85354ec311cb14
ioulasri <imad.oulasri@iu-study.org>
2023-03-25 16:49:15 +0000

    Updated and fixed bugs on _printf function

--- 0-handle_string.c
+++ 0-handle_string.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <unistd.h>
 
 /**
  * _strlen - returns the length of a given string
--- main.h
+++ main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 #include <stdarg.h>
 #include <stdio.h>
+#include <unistd.h>
 
 void _putchar(char c);
 int _printf(const char *format, ...);
