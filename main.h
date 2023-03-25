4f13b87d79687288234e3c84e820b04d850355ba
ioulasri <imad.oulasri@iu-study.org>
2023-03-25 14:17:29 +0000

    Updating recreated printf

--- main.h
+++ main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 
 void _putchar(char c, int *sum);
+int _putchar(char c);
 void print_int(int num, int *sum);
 void print_int(int num, int *sum);
 void handel_spec(char c, int * sum, va_list args);
