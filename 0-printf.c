16e68d3306bc2db55437580691363b644a5cf684
ioulasri <imad.oulasri@iu-study.org>
2023-03-25 14:17:02 +0000

    Updating recreated printf

--- 0-printf.c
+++ 0-printf.c
@@ -34,7 +34,7 @@ void print_int(int num, int *sum)
         print_int((num % 10), sum);
     }
 }
-void print_str(char *str, int *sum)
+c
 {
     int i = 0;
 
@@ -91,26 +91,4 @@ int _printf(char *str, ...)
     }
     va_end(args);
     return (sum);
-}
-
-int main(void)
-{
-    int len;
-    int len2;
-    unsigned int ui;
-    void *addr;
-
-    len = _printf("Let's try to printf a simple sentence.\n");
-    len2 = printf("Let's try to printf a simple sentence.\n");
-    len = _printf("Percent:[%%]\n");
-    len2 = printf("Percent:[%%]\n");
-    _printf("Len:[%d]\n", len);
-    printf("Len:[%d]\n", len2);
-     printf("Length:[%d, %i]\n", len2, len2);
-    _printf("Negative:[%d]\n", -762534);
-    printf("Negative:[%d]\n", -762534);
-    _printf("Character:[%c]\n", 'H');
-    printf("Character:[%c]\n", 'H');
-    _printf("String:[%s]\n", "I am a string !");
-    printf("String:[%s]\n", "I am a string !");
 }
---
+++ main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void _putchar(char c, int *sum);
+void print_int(int num, int *sum);
+void print_int(int num, int *sum);
+void handel_spec(char c, int * sum, va_list args);
+int _printf(char *str, ...);
+
+#endif
