2a94dd07060cc6fd42aba6037bc2716a9fb8b33f
ioulasri <imad.oulasri@iu-study.org>
2023-03-25 16:00:21 +0000

    Updated and fixed bugs on _printf function

--- 0-printf.c
+++ 0-printf.c
@@ -1,94 +1,168 @@
-#include <stdio.h>
+#include "main.h"
 #include <stdarg.h>
 #include <unistd.h>
 
-void _putchar(char c, int *sum)
+/**
+ * _strlen - returns the length of a given string
+ * @s: the string
+ * Return: the length of given string
+ */
+
+int _strlen(char *s)
 {
-    write(1, &c, 1),
-    (*sum)++;
+	int i;
+
+	i = 0;
+	while (s[i])
+	{
+		i++;
+	}
+	return (i);
 }
 
-void print_int(int num, int *sum)
-{
-    static int sign;
-    char c;
+/**
+ * _putchar - prints a character to stdout
+ * @c: the char to print
+ */
 
-    if (num < 0)
-    {
-        write(1, "-", 1);
-        (*sum)++;
-        num *= -1;
-    }
-    else
-        num *= 1;
-    
-    if (num < 10)
-    {
-        c = num + '0';
-        write(1, &c, 1);
-        (*sum)++;
-    }
-    else
-    {
-        print_int((num / 10), sum);
-        print_int((num % 10), sum);
-    }
-}
-void print_str(char *str, int *sum)
+void _putchar(char c)
 {
-    int i = 0;
+	write(1, &c, 1);
+}
 
-    while (str[i])
-    {
-        write(1, &str[i], 1);
-        i++;
-        (*sum)++;
-    }
-}
-void handel_spec(char c, int * sum, va_list args)
-{
-    if (c == 'd' || c == 'i')
-    {
-        int num = va_arg(args, int);
-        print_int(num, sum);
-    }
-    else if (c == 'c')
-    {
-        char c = va_arg(args, int);
-        _putchar(c, sum);
-    }
-    else if (c == '%')
-    {
-        char c = '%';
-        _putchar(c, sum);
-    }
-    else if (c == 's')
-    {
-        char *str = va_arg(args, char *);
-        print_str(str, sum);
-    }
-}
-int _printf(char *str, ...)
-{
-    int i = 0;
-    int sum = 0;
-    va_list args;
+/**
+ * _num_count - counts the length of a number
+ * @num: the number
+ * Return: the length of the number
+ */
 
-    va_start(args, str);
-    while(*(str + i))// str[i]
-    {
-        if (str[i] != '%')
-        {
-            _putchar(str[i], &sum);
-            i++;
-        }
-        else
-        {
-              handel_spec(str[i + 1], &sum, args);
-              i += 2;
-        }
-        
-    }
-    va_end(args);
-    return (sum);
+int _num_count(int num)
+{
+	int count = 0;
+
+	while (num > 0)
+	{
+		num /= 10;
+		count++;
+	}
+	return (count);
 }
+
+/**
+ * print_number - prints a given number
+ * @n: the number to be printed
+ */
+
+void print_number(int n)
+{
+	if (n == -2147483648)
+	{
+		_putchar('-');
+		_putchar('2');
+		print_number(147483648);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+		print_number(n);
+	}
+	else if (n > 9)
+	{
+		print_number(n / 10);
+		print_number(n % 10);
+	}
+	else
+		_putchar(n + 48);
+}
+
+/**
+ * _puts - prints a given string
+ * @str: the string
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
+
+/**
+ * handle_format - handles given format specifiers and prints the args
+ * @c: the format specifier
+ * @arg: the arguments
+ */
+
+int handle_format(char c, va_list arg)
+{
+	if (c == 'd' || c == 'i')
+	{
+		int num = va_arg(arg, int);
+
+		print_number(num);
+		return (_num_count(num));
+	}
+	else if (c == 'c')
+	{
+		char e = va_arg(arg, int);
+
+		_putchar(e);
+		return (1);
+	}
+	else if (c == 's')
+	{
+		char *s = va_arg(arg, char *);
+		
+		_puts(s);
+		return (_strlen(s));
+	}
+
+	else if (c == '%')
+	{	
+		_putchar('%');
+	}
+	return (1);
+}
+
+/**
+ * _printf - prints string integer and char using format specifiers
+ * @format: the first argument containing the string and format specifiers
+ * Return: the length of the printed string.
+ */
+
+int _printf(const char *format, ...)
+{
+	int i = 0;
+	int printed_chars = 0;
+	int re;
+	va_list arg;
+
+	va_start(arg, format);
+
+	while (format[i])
+	{
+		if (format[i] != '%')
+		{
+			_putchar(format[i]);
+			i++;
+			printed_chars += 1;
+		}
+		else
+		{
+			re = handle_format(format[i + 1], arg);
+			printed_chars += re;
+			i += 2;
+		}
+	}
+
+	va_end(arg);
+	return (printed_chars);
+}
+
--- main.h
+++ main.h
@@ -1,10 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-void __putchar(char c, int *sum);
-void print_int(int num, int *sum);
-void print_int(int num, int *sum);
-void handel_spec(char c, int * sum, va_list args);
-int _printf(char *str, ...);
+void _putchar(char c);
+int _printf(const char *format, ...);
 
 #endif
