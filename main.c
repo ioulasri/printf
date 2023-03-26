9b71537fdd7519243d726097bc59e8fdb2cfae91
ioulasri <imad.oulasri@iu-study.org>
2023-03-26 15:34:46 +0000

    Debugged and pushed printf file

--- main.c
+++ main.c
@@ -1,13 +1,18 @@
 #include "main.h"
 
+/**
+ * main - entry point
+ * Return: 0
+ */
+
 int main(void)
 {
 	_printf("Let's try to printf a simple sentence.\n");
 	printf("Let's try to printf a simple sentence.\n");
-    _printf("Character:[%c] [%c] [%c]\n", 'H', 'a', 'r');
-    printf("Character:[%c] [%c]\n", 'H', 'a');
-    _printf("String:[%s]\n", (void*)NULL);
-    printf("String:[%s]\n", (void*)NULL);
+	_printf("Character:[%c] [%c] [%c]\n", 'H', 'a', 'r');
+	printf("Character:[%c] [%c]\n", 'H', 'a');
+	_printf("String:[%s]\n", (void *)NULL);
+	printf("String:[%s]\n", (void *)NULL);
 	_printf("Percent:[%%]\n");
 	printf("Percent:[%%]\n");
 	_printf("Print this number [%d]\n", -2147483648);
