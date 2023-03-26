a449e004a730d89b2b90fa9ccc534edfe992020b
ioulasri <imad.oulasri@iu-study.org>
2023-03-26 15:27:35 +0000

    Debugged and pushed printf file

--- main.c
+++ main.c
@@ -6,8 +6,8 @@ int main(void)
 	printf("Let's try to printf a simple sentence.\n");
     _printf("Character:[%c] [%c] [%c]\n", 'H', 'a', 'r');
     printf("Character:[%c] [%c]\n", 'H', 'a');
-    _printf("String:[%s]\n", "I am a string !");
-    printf("String:[%s]\n", "I am a string !");
+    _printf("String:[%s]\n", (void*)NULL);
+    printf("String:[%s]\n", (void*)NULL);
 	_printf("Percent:[%%]\n");
 	printf("Percent:[%%]\n");
 	_printf("Print this number [%d]\n", -2147483648);
