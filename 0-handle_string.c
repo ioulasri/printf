ee46bc3354b6e3489896cefcfecb94c34020193b
badrbnh <badrechahide@gmail.com>
2023-03-28 02:00:41 +0000

    Debugged

--- 0-handle_string.c
+++ 0-handle_string.c
@@ -18,36 +18,32 @@ int _strlen(char *s)
 	return (i);
 }
 
-
 /**
  * _putchar - prints a character to stdout
  * @c: the char to print
- * Return: 1 on success, -1 on failure
+ * @sum: the number of printed char
  */
+
 void _putchar(char c, int *sum)
 {
 	write(1, &c, 1);
 	(*sum)++;
 }
+
 /**
  * print_str - prints a given string
  * @str: the string
  * @sum: the sum of printed characters
- * Return: 0 if successful, -1 if str is NULL
  */
-int print_str(char *str, int *sum)
+
+void print_str(char *str, int (*sum))
 {
 	int i = 0;
 
-	if (str == NULL)
-		return (-1);
-
-	while (str[i])
+	while (str && str[i])
 	{
 		write(1, &str[i], 1);
 		(*sum)++;
 		i++;
 	}
-
-	return (0);
 }
