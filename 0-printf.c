f06c99934f955f742eaa531dce7d9c9263e7e784
ioulasri <115421891+ioulasri@users.noreply.github.com>
2023-03-28 13:27:33 +0000

    Update 0-printf.c

--- 0-printf.c
+++ 0-printf.c
@@ -1,52 +1,89 @@
 #include "main.h"
 
 /**
- * _printf - prints formatted output to the standard output stream
- * @format: the format string
- * Return: the number of characters printed (excluding the null byte)
+ * handle_spec - handles given format specifiers and prints the args
+ * @c: the format specifier
+ * @args: the arguments
+ * @sum: the number of characters printed
+ * Return: returns the number of characters printed
  */
+
+void handle_spec(char c, int *sum, va_list args)
+{
+	if (c == 'd' || c == 'i')
+	{
+		int num = va_arg(args, int);
+
+		print_int(num, sum);
+	}
+	else if (c == 'c')
+	{
+		char ch = va_arg(args, int);
+
+		_putchar(ch, sum);
+	}
+	else if (c == '%')
+	{
+		char ch = '%';
+
+		_putchar(ch, sum);
+	}
+	else if (c == 's')
+	{
+		char *str = va_arg(args, char *);
+
+		if (str == NULL)
+			print_str("(null)", sum);
+		else
+			print_str(str, sum);
+	}
+	else if (c == 'b')
+	{
+		int num = va_arg(args, int);
+
+		print_binary(num, sum);
+	}
+	else
+	{
+		_putchar('%', sum);
+		_putchar(c, sum);
+	}
+}
+
+/**
+ * _printf - prints string integer and char using format specifiers
+ * @format: the first argument containing the string and format specifiers
+ * Return: the length of the printed string or -1 on failure.
+ */
+
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int count = 0;
+	int i = 0, sum = 0;
+	va_list args;
 
-    va_start(args, format);
+	if (!format || !format[0])
+		return (-1);
+	va_start(args, format);
 
-    while (*format)
-    {
-        if (*format == '%')
-        {
-            format++;
+	while (format[i])
+	{
+		if (format[i] != '%')
+		{
+			_putchar(format[i], &sum);
+			i++;
+		}
+		else if (format[i + 1] == '\0')
+		{
+			va_end(args);
+			return (-1);
+		}
+		else
+		{
+			handle_spec(format[i + 1], &sum, args);
+			i += 2;
+		}
+	}
 
-            if (*format == '%')
-            {
-                count += _putchar('%');
-            }
-            else if (*format == 'c')
-            {
-                char c = va_arg(args, int);
-                count += _putchar(c);
-            }
-            else if (*format == 's')
-            {
-                char *s = va_arg(args, char *);
-                count += _print_string(s);
-            }
-            else
-            {
-                count += _putchar('%');
-                count += _putchar(*format);
-            }
-        }
-        else
-        {
-            count += _putchar(*format);
-        }
-
-        format++;
-    }
-
-    va_end(args);
-
-    return (count);
+	va_end(args);
+	return (sum);
 }
