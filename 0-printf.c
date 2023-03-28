e9b8f653e09373a800255f3c710870ea4e3d62b7
badrbnh <badrechahide@gmail.com>
2023-03-28 00:19:00 +0000

    Debugged

--- 0-printf.c
+++ 0-printf.c
@@ -52,31 +52,46 @@ void handle_spec(char c, int *sum, va_list args)
 /**
  * _printf - prints string integer and char using format specifiers
  * @format: the first argument containing the string and format specifiers
- * Return: the length of the printed string.
+ * Return: the length of the printed string or -1 on failure.
  */
 
 int _printf(const char *format, ...)
 {
-	int i = 0;
-	int sum = 0;
+	if (!format || !format[0])
+		return (-1);
+
+	int i = 0, sum = 0;
 	va_list args;
 
 	va_start(args, format);
-	if (!format || !format[0])
-		return (-1);
-	while (*(format + i) && format)
+
+	while (format[i])
 	{
 		if (format[i] != '%')
 		{
 			_putchar(format[i], &sum);
 			i++;
 		}
+		else if (format[i + 1] == '\0')
+		{
+			va_end(args);
+			return (-1);
+		}
 		else
 		{
+			if (format[i + 1] == '%')
+			{
+				_putchar('%', &sum);
+				i += 2;
+				continue;
+			}
+
 			handle_spec(format[i + 1], &sum, args);
 			i += 2;
 		}
 	}
+
 	va_end(args);
 	return (sum);
 }
+
