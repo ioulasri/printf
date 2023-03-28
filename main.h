36eccd13e87d598954407d32b0e703304803b2f4
ioulasri <115421891+ioulasri@users.noreply.github.com>
2023-03-28 13:28:28 +0000

    Update main.h

--- main.h
+++ main.h
@@ -1,12 +1,17 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 #include <stdarg.h>
-#include <unistd.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
 
-int _putchar(char c);
-int _print_string(char *str);
 int _printf(const char *format, ...);
+void _putchar(char c, int *sum);
+void handle_spec(char c, int *sum, va_list args);
+int _strlen(char *s);
+void print_str(char *str, int (*sum));
+int _num_count(int num);
+void print_int(int num, int *sum);
+void print_binary(int num, int *sum);
 
-#endif /* MAIN_H */
+#endif
